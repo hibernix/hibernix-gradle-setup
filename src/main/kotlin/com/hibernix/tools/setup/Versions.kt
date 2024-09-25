1ce2a09fbb26c5e87db0828e4b96653cb1af7f07
Jozef Krupik <jozef.krupik@hibernix.com>
2024-09-25 14:37:35 +0800

    Update dependencies

--- src/main/kotlin/com/hibernix/tools/setup/Versions.kt
+++ src/main/kotlin/com/hibernix/tools/setup/Versions.kt
@@ -8,8 +8,8 @@ import org.gradle.api.Project
 enum class Versions(var version: String, val property: String) {
     Kotlin("2.0.20", "project.versions.kotlin"),
     Ksp("2.0.20-1.0.25", "project.versions.ksp"),
-    Coroutines("1.9.0-RC.2", "project.versions.coroutines"),
-    Serialization("1.7.2", "project.versions.serialization"),
+    Coroutines("1.9.0", "project.versions.coroutines"),
+    Serialization("1.7.3", "project.versions.serialization"),
     Datetime("0.6.1", "project.versions.datetime"),
     Logging("0.2.1", "project.versions.logging"),
     ;
@@ -18,6 +18,7 @@ enum class Versions(var version: String, val property: String) {
         project.findProperty(property)?.toString() ?: version
 }
 
+/*
 object LibVersions {
     var kotlin = "2.0.0"
     var ksp = "2.0.0-1.0.22"
@@ -30,3 +31,4 @@ object LibVersions {
 fun libVersions(init: LibVersions.() -> Unit) {
     init(LibVersions)
 }
+*/
